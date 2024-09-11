961e4340b31ca02a325bc8a08e33aa8bd9d05cf9
j0e-mo <119964946+j0e-mo@users.noreply.github.com>
2024-09-11 18:35:15 +0300

    Add files via upload

---
+++ AUTOSAR_Project_WS/AUTOSAR_Project_WS/AUTOSAR_Project/Debug/Led.d
@@ -0,0 +1,42 @@
+# FIXED
+
+Led.obj: ../Led.c
+Led.obj: ../Port.h
+Led.obj: ../Port_Types.h
+Led.obj: ../Std_Types.h
+Led.obj: ../Platform_Types.h
+Led.obj: ../Compiler.h
+Led.obj: ../Port_Cfg.h
+Led.obj: ../Port_Reg.h
+Led.obj: ../Common_Macros.h
+Led.obj: ../Dio.h
+Led.obj: ../Dio_Cfg.h
+Led.obj: ../Led.h
+Led.obj: ../Led_Cfg.h
+
+../Led.c:
+
+../Port.h:
+
+../Port_Types.h:
+
+../Std_Types.h:
+
+../Platform_Types.h:
+
+../Compiler.h:
+
+../Port_Cfg.h:
+
+../Port_Reg.h:
+
+../Common_Macros.h:
+
+../Dio.h:
+
+../Dio_Cfg.h:
+
+../Led.h:
+
+../Led_Cfg.h:
+
---
+++ AUTOSAR_Project_WS/AUTOSAR_Project_WS/AUTOSAR_Project/Debug/Port.d
@@ -0,0 +1,33 @@
+# FIXED
+
+Port.obj: ../Port.c
+Port.obj: ../Port.h
+Port.obj: ../Port_Types.h
+Port.obj: ../Std_Types.h
+Port.obj: ../Platform_Types.h
+Port.obj: ../Compiler.h
+Port.obj: ../Port_Cfg.h
+Port.obj: ../Port_Reg.h
+Port.obj: ../Common_Macros.h
+Port.obj: ../Det.h
+
+../Port.c:
+
+../Port.h:
+
+../Port_Types.h:
+
+../Std_Types.h:
+
+../Platform_Types.h:
+
+../Compiler.h:
+
+../Port_Cfg.h:
+
+../Port_Reg.h:
+
+../Common_Macros.h:
+
+../Det.h:
+
